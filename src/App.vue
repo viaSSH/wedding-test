598a8b8d25aa03c3721b4f50c88229343434f5bb
viassh <hanseung0609@gmail.com>
2021-02-16 23:52:51 +0900

    dev start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Viewer />
+    <Editer />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Viewer from "./components/Viewer.vue"
+import Editer from "./components/Editer.vue"
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Viewer,
+    Editer
   }
 };
 </script>
